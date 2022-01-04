c79e2fc365cc43b388929b9136c8eb192755164f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-01-04 12:14:16 -0500

    Implement slice based stack

---
+++ slice_stack/slice_stack_test.go
@@ -0,0 +1,27 @@
+package slice_stack
+
+import (
+	"go-exercises/assert"
+	"testing"
+)
+
+func TestSliceStack(t *testing.T) {
+	stack := NewSliceStack()
+
+	stack.Push(1)
+	stack.Push(2)
+
+	result := stack.Read()
+	expected := 2
+	assert.Equals(t, "Stack.Read()", "Given two elements pushed", "Should read last element in stack", result, expected)
+
+	stack.Push(3)
+	result = stack.Pop()
+	expected = 3
+	assert.Equals(t, "Stack.Pop()", "Given three elements pushed & one popped", "Should return element popped", result, expected)
+
+	result = stack.Read()
+	expected = 2
+	assert.Equals(t, "Stack.Read()", "Given three elements pushed & one popped", "Should read second element", result, expected)
+
+}
