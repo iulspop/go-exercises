ec6334fa75e5a7ab353db9444bf7810ba9c0c2a5
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-01-06 14:25:38 -0500

    Clean up tests

--- combos/combos_test.go
+++ combos/combos_test.go
@@ -9,14 +9,6 @@ func TestCombos(t *testing.T) {
 	result := Combos([]int {5}, map[string]bool {})
 	expected := [][]int {{5}, {1, 4}, {1, 1, 3}, {1, 1, 1, 2}, {1, 1, 1, 1, 1},  {1, 2, 2}, {2, 3}}
 	assert.Equals(t, "Combos()", "Given a number", "Should return all possible sets that sum to that number", result, expected)
-
-	// result = Combos([]int {1,1,1,1,1})
-	// expected = [][]int {{1,1,1,1,1}}
-	// assert.Equals(t, "Combos()", "Given a number", "Should return all possible sets that sum to that number", result, expected)
-
-	// result = Combos([]int {1,1,1,2})
-	// expected = [][]int {{1,1,1,1,1}}
-	// assert.Equals(t, "Combos()", "Given a number", "Should return all possible sets that sum to that number", result, expected)
 }
 
 func TestRemove(t *testing.T)  {
