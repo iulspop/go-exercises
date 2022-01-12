fa815414b87806810ecda8f4616b34397b20d3c1
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-01-12 18:10:10 -0500

    Fix test msgs

--- find_peak_element/find_peak_element_test.go
+++ find_peak_element/find_peak_element_test.go
@@ -8,17 +8,17 @@ import (
 func TestFindPeakElement(t *testing.T) {
 	result := FindPeakElement([]int {0})
 	expected := 0
-	assert.Equals(t, "FindPeakElement()", "Given two slices of integers", "Should return slice of common integers", result, expected)
+	assert.Equals(t, "FindPeakElement()", "", "", result, expected)
 
 	result = FindPeakElement([]int {1, 2})
 	expected = 1
-	assert.Equals(t, "FindPeakElement()", "Given two slices of integers", "Should return slice of common integers", result, expected)
+	assert.Equals(t, "FindPeakElement()", "", "", result, expected)
 
 	result = FindPeakElement([]int {2, 1})
 	expected = 0
-	assert.Equals(t, "FindPeakElement()", "Given two slices of integers", "Should return slice of common integers", result, expected)
+	assert.Equals(t, "FindPeakElement()", "", "", result, expected)
 
 	result = FindPeakElement([]int {1,2,1,3,5,6,4})
 	expected = 5
-	assert.Equals(t, "FindPeakElement()", "Given two slices of integers", "Should return slice of common integers", result, expected)
+	assert.Equals(t, "FindPeakElement()", "", "", result, expected)
 }
